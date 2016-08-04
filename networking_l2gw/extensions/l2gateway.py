305127327a8ce2039d732ea71cfce5e68b4ac77e
Gary Kotton <gkotton@vmware.com>
2016-08-04 02:57:33 -0700

    Use neutron-lib add_validator for registration
    As per [1], neutron-lib 0.3.0 provides a public add/get API for
    local validator registration/access. This API is preferred over
    directly accessing the validators dict module-level attribute
    that's done today when adding a new validator and in fact
    direct access to the validators dict is deprecated.
    
    This patch changes all l2gw's usage of the validators dict
    to use the public API.
    
    [1] https://review.openstack.org/#/c/324090/
    
    Related-Bug: #1584237
    
    Change-Id: I8e3af59d09b14bf208f8b87bec91772a672b6764

--- networking_l2gw/extensions/l2gateway.py
+++ networking_l2gw/extensions/l2gateway.py
@@ -18,6 +18,8 @@ from neutron.api import extensions
 from neutron.api.v2 import attributes
 from neutron.api.v2 import resource_helper
 
+from neutron_lib.api import validators
+
 import networking_l2gw.extensions
 from networking_l2gw.services.l2gateway.common import constants
 from networking_l2gw.services.l2gateway.common import l2gw_validators
@@ -42,7 +44,7 @@ RESOURCE_ATTRIBUTE_MAP = {
 }
 
 validator_func = l2gw_validators.validate_gwdevice_list
-attributes.validators['type:l2gwdevice_list'] = validator_func
+validators.add_validator('l2gwdevice_list', validator_func)
 
 
 class L2gateway(extensions.ExtensionDescriptor):
