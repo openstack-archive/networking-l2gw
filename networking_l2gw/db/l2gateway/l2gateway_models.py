5591b7cffa977a5deadad4792f8fc21b7ff3014e
Henry Gessau <gessau@gmail.com>
2016-08-11 23:22:34 -0400

    Remove temporary local HasProject
    Now that the tenant_id rename has merged in Neutron we can
    remove the temporary work-around.
    
    Also switch from the deprecated models_v2.HasId to the relocated
    model_base.HasId
    
    Related Blueprint: keystone-v3
    
    Change-Id: I6e480be8f3fb725b9ff74309288f9c7faef84b68

--- networking_l2gw/db/l2gateway/l2gateway_models.py
+++ networking_l2gw/db/l2gateway/l2gateway_models.py
@@ -14,39 +14,13 @@
 # under the License.
 
 import sqlalchemy as sa
-from sqlalchemy.ext import declarative
 from sqlalchemy import orm
 
-from neutron.api.v2 import attributes as attr
 from neutron.db import model_base
-from neutron.db import models_v2
 
 
-class HasProject(object):
-    # NOTE(dasm): Temporary solution!
-    # Remove when I87a8ef342ccea004731ba0192b23a8e79bc382dc is merged.
-
-    project_id = sa.Column(sa.String(attr.TENANT_ID_MAX_LEN), index=True)
-
-    def __init__(self, *args, **kwargs):
-        # NOTE(dasm): debtcollector requires init in class
-        super(HasProject, self).__init__(*args, **kwargs)
-
-    def get_tenant_id(self):
-        return self.project_id
-
-    def set_tenant_id(self, value):
-        self.project_id = value
-
-    @declarative.declared_attr
-    def tenant_id(cls):
-        return orm.synonym(
-            'project_id',
-            descriptor=property(cls.get_tenant_id, cls.set_tenant_id))
-
-
-class L2GatewayConnection(model_base.BASEV2, HasProject,
-                          models_v2.HasId):
+class L2GatewayConnection(model_base.BASEV2, model_base.HasProject,
+                          model_base.HasId):
     """Define an l2 gateway connection between a l2 gateway and a network."""
     l2_gateway_id = sa.Column(sa.String(36),
                               sa.ForeignKey('l2gateways.id',
@@ -59,7 +33,7 @@ class L2GatewayConnection(model_base.BASEV2, HasProject,
                                           network_id),)
 
 
-class L2GatewayInterface(model_base.BASEV2, models_v2.HasId):
+class L2GatewayInterface(model_base.BASEV2, model_base.HasId):
     """Define an l2 gateway interface."""
     interface_name = sa.Column(sa.String(255))
     device_id = sa.Column(sa.String(36),
@@ -69,7 +43,7 @@ class L2GatewayInterface(model_base.BASEV2, models_v2.HasId):
     segmentation_id = sa.Column(sa.Integer)
 
 
-class L2GatewayDevice(model_base.BASEV2, models_v2.HasId):
+class L2GatewayDevice(model_base.BASEV2, model_base.HasId):
     """Define an l2 gateway device."""
     device_name = sa.Column(sa.String(255), nullable=False)
     interfaces = orm.relationship(L2GatewayInterface,
@@ -81,7 +55,7 @@ class L2GatewayDevice(model_base.BASEV2, models_v2.HasId):
                               nullable=False)
 
 
-class L2Gateway(model_base.BASEV2, models_v2.HasId, HasProject):
+class L2Gateway(model_base.BASEV2, model_base.HasId, model_base.HasProject):
     """Define an l2 gateway."""
     name = sa.Column(sa.String(255))
     devices = orm.relationship(L2GatewayDevice,
